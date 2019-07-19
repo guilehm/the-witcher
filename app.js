4d4b00940642694d6dcb7ce187b09a26177de7d1
Guilherme Luis <guile.hm@hotmail.com>
2019-07-18 22:10:10 -0300

    create app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+
+const DEBUG = process.env.DEBUG
+const app = new express()
+
+app.use(morgan('short'))
+app.use(helmet())
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    let message = DEBUG ? 'Starting development server on port' : 'App listening on port'
+    console.log(message, `${PORT}`)
+})
