a919c2474b2a0ce6c9f790b6abb65eaf7fa4614c
Guilherme Luis <guile.hm@hotmail.com>
2019-09-22 14:29:36 -0300

    setup jshint

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const morgan = require('morgan')
-const helmet = require('helmet')
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
-const DEBUG = process.env.DEBUG
-const app = new express()
+const DEBUG = process.env.DEBUG || 1;
+const app = new express();
 
-app.use(morgan('short'))
-app.use(helmet())
+app.use(morgan('short'));
+app.use(helmet());
 
 const cleanStatsController = require('./controllers/clean-stats-controller')
 
-app.get('/', cleanStatsController)
+app.get('/', cleanStatsController);
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    let message = DEBUG ? 'Starting development server on port' : 'App listening on port'
-    console.log(message, `${PORT}`)
-})
+    let message = DEBUG ? 'Starting development server on port' : 'App listening on port';
+    console.log(message, `${PORT}`);
+});
