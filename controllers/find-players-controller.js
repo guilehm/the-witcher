0805dcc4c5d4eba8f61f2f43f65a5e955f5ef5db
Guilherme Luis <guile.hm@hotmail.com>
2019-09-22 16:10:58 -0300

    create find players controller

---
+++ controllers/find-players-controller.js
@@ -0,0 +1,8 @@
+const { findPlayers } = require('../services/skout-service');
+
+module.exports = async (req, res) => {
+    let name = req.params.name;
+    let players = await findPlayers(name);
+    let status = players.results ? 200 : 500;
+    return res.status(status).json(players.results);
+};
