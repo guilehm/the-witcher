2393962fc45125508621f01c24b35c5427b8ee45
Guilherme Luis <guile.hm@hotmail.com>
2019-09-22 14:57:02 -0300

    create get fortnite function

--- services/skout-service.js
+++ services/skout-service.js
@@ -4,7 +4,7 @@ const SKOUT_CLIENT_ID = process.env.SKOUT_CLIENT_ID;
 const SKOUT_CLIENT_SECRET = process.env.SKOUT_CLIENT_SECRET;
 
 async function getScoutClient() {
-    const ScoutClient = await Scout.configure({
+    let ScoutClient = await Scout.configure({
         clientId: SKOUT_CLIENT_ID,
         clientSecret: SKOUT_CLIENT_SECRET,
         scope: "public.read"
@@ -12,5 +12,11 @@ async function getScoutClient() {
     return ScoutClient;
 }
 
+async function getFortnite() {
+    let ScoutClient = await getScoutClient();
+    let titles = ScoutClient.titles.list();
+    let fortnite = titles.find(t => t.slug === "fortnite");
+    return fortnite;
+}
 
-module.exports = { getScoutClient };
+module.exports = { getScoutClient, getFortnite };
