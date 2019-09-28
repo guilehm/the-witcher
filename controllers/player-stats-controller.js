d178112f1bc43736159a4df24ce89fb4ad25d3bd
guilherme <guile.hm@hotmail.com>
2019-09-28 18:39:25 -0300

    save metadata

--- controllers/player-stats-controller.js
+++ controllers/player-stats-controller.js
@@ -1,8 +1,16 @@
 const { getPlayerStats } = require('../services/skout-service');
+const Metadata = require('../database/models/metadata');
 
 module.exports = async (req, res) => {
     let playerId = req.params[0];
     let stats = await getPlayerStats(playerId);
     let status = stats.id ? 200 : 400;
     res.status(status).json(stats);
+
+    // save this for metadata endpoint
+    if (status === 200) {
+        Metadata.collection.drop()
+        let metadatas = stats.stats.map(data => data['metadata'])
+        Metadata.collection.insert(metadatas)
+    }
 };
