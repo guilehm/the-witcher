085fc83e8d6f3b0fd3855687f1b0f26fa6ad4bb8
guilherme <guile.hm@hotmail.com>
2019-09-28 11:27:07 -0300

    enable cors

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const friendListController = require('./controllers/friend-list-controller');
 const findPlayersController = require('./controllers/find-players-controller');
 const playerStatsController = require('./controllers/player-stats-controller');
 
+
+// enable cors
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.get('/fortnite/', getGameController);
 app.get('/friends/', friendListController);
 app.get('/players/:name/', findPlayersController);
