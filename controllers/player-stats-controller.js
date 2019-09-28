7e45cb05f3d8d75a05885ae8d678f14997ddc559
guilherme <guile.hm@hotmail.com>
2019-09-28 13:41:03 -0300

    get param from regex instead of named args

--- controllers/player-stats-controller.js
+++ controllers/player-stats-controller.js
@@ -1,7 +1,7 @@
 const { getPlayerStats } = require('../services/skout-service');
 
 module.exports = async (req, res) => {
-    let playerId = req.params.id;
+    let playerId = req.params[0];
     let stats = await getPlayerStats(playerId);
     let status = stats.id ? 200 : 400;
     res.status(status).json(stats);
