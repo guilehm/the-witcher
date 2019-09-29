17561ba1eb27085388f64bc7201d360b5bf54162
guilherme <guile.hm@hotmail.com>
2019-09-29 09:29:31 -0300

    improve query

--- controllers/metadata-list-controller.js
+++ controllers/metadata-list-controller.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
         return res.json(data);
     };
 
-    await Metadata.find({})
+    await Metadata.find({}, {key: 1, name: 1, _id: 0})
         .then(handleResponse)
         .catch(handleResponse);
 
