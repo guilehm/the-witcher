ce0d4ad99fd175d04aa247f32a0e65c4665ba709
Guilherme Luis <guile.hm@hotmail.com>
2019-09-30 22:14:14 -0300

    create save segment function

--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,23 @@
 const Metadata = require('../database/models/metadata');
+const Segment = require('../database/models/segment');
 
 
 function saveMetadata(stats) {
     let metadatas = stats.stats.map(data => data['metadata'])
     metadatas.forEach(data => {
         Metadata.findOneAndUpdate({ key: data.key }, data, { upsert: true })
-            .then(m => console.log(`Success updating ${m.key}`))
+            .then(m => console.log(`Success updating metadata ${m.key}`))
             .catch(e => console.log(e))
     })
 }
 
-module.exports = { saveMetadata }
+function saveSegment(stats) {
+    let segments = stats.segments.map(segment => segment['metadata'][0])
+    segments.forEach(segment => {
+        Segment.findOneAndUpdate({ value: segment.value }, segment, { upsert: true })
+            .then(s => console.log(`Success updating segment ${s.value}`))
+            .catch(e => console.log(e))
+    })
+}
+
+module.exports = { saveMetadata, saveSegment }
